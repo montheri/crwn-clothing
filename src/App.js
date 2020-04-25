00359c94cda62845252097d75f716e014d6e2696
Monther idkaidek <montheri@msn.com>
2020-04-26 01:04:43 +0300

    create sign up form and integrated it with firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import SingInAndSignUpPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
@@ -19,9 +19,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: userRef.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)});
+        });
+      }
+      else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
@@ -36,7 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/SingIn' component={SingInAndSignUpPage}/>
+          <Route exact path='/SingIn' component={SignInAndSignUpPage}/>
           </Switch>
       </div>
     );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.styles.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
+
+
+const SignInAndSignOut = () => (
+    <div className='sing-in-sing-up'>
+        <SignIn />
+        <SignUp />
+    </div>
+
+);
+
+export default SignInAndSignOut 
