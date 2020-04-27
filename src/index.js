99706c4447f9f3a9a126235e81700acb5a980231
Monther idkaidek <montheri@msn.com>
2020-04-27 22:48:35 +0300

    fix conflicts

--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
-import {Provider} from 'react-redux'
-
-
-ReactDOM.render(
-  <Provider>
-=======
 import { Provider } from 'react-redux'
-
-import store from './redux/store'
 
 
 ReactDOM.render(
   <Provider store={store}>
->>>>>>> 125017d06aa19756aa5a8945812329bab6633eca
     <BrowserRouter>
       <App />
     </BrowserRouter>
